e550100d7823aa07559c746839c729adffd1207a
Dhananjay Singh <dhananjay.ansh2242@gmail.com>
2025-09-21 15:54:57 +0530

    Initial commit

--- src/main/java/com/chat/app/config/WebSocketConfig.java
+++ src/main/java/com/chat/app/config/WebSocketConfig.java
@@ -6,7 +6,6 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-import io.micrometer.common.lang.NonNull;
 
 
 @Configuration
